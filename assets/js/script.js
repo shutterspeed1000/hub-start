634db48be04f51d35410d94f084731f76e68f646
Sean Worrell <spworrell@gmail.com>
2023-08-30 14:51:39 -0400

    survey

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// $(".surveyRandom").on("click", function () {
+//   let lotto = Math.floor(Math.random() * 2) + 1;
+
+//   alert(lotto);
+
+  
+
+//   if (lotto == 2) {
+
+//     let takesurv = confirm(`Would you like to take a brief survey about your visit today?`)
+// alert(takesurv)
+// if (takesurv == true) {
+
+//     $(`#main`).replaceWith(
+
+        
+
+// `
+// <section id="main">
+
+// <iframe src="https://www.surveymonkey.com/r/TSGLVDR" height="50%" width="100%" title="Survey"></iframe>
+
+// </section>
+
+// `
+
+
+
+//     )
+// }
+
+// }
+
+// })
