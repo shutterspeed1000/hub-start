fba1cb3c20ac5864d991e0081e8ab50e6242247e
Sean Worrell <spworrell@gmail.com>
2024-08-20 11:01:27 -0400

    surveyadded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,3 @@
-// $(".surveyRandom").on("click", function () {
-//   let lotto = Math.floor(Math.random() * 2) + 1;
-
-//   alert(lotto);
-
-  
-
-//   if (lotto == 2) {
-
-//     let takesurv = confirm(`Would you like to take a brief survey about your visit today?`)
-// alert(takesurv)
-// if (takesurv == true) {
-
-//     $(`#main`).replaceWith(
-
-        
-
-// `
-// <section id="main">
-
-// <iframe src="https://www.surveymonkey.com/r/TSGLVDR" height="50%" width="100%" title="Survey"></iframe>
-
-// </section>
-
-// `
-
-
-
-//     )
-// }
-
-// }
-
-// })
+setTimeout(function () {
+  $(".survey").css("text.color", "red");
+}, 1000);
