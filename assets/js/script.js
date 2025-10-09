355f42b7d014e8bd046f59901db45739cd8b73b6
Sean Worrell <spworrell@gmail.com>
2025-10-09 08:28:50 -0400

    ReConnect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 setTimeout(function () {
   $(".survey").css("text.color", "red");
 }, 1000);
+
+function checkReconnectCode(event) {
+    event.preventDefault();
+    const code = prompt("Please enter the access code:");
+    if (code === "1801") {
+        window.open("https://reconnect.commerce.fl.gov", "_blank");
+    } else {
+        alert("Incorrect code. Access denied.");
+    }
+}
